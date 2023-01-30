9211f2b51e33037fc3310f427a2dbe27368dc506
t0in4 <t0in4@yahoo.com>
2023-01-30 20:18:41 +0900

    20:18

--- src/overridingandoverloading/SubClass.java
+++ src/overridingandoverloading/SubClass.java
@@ -8,7 +8,13 @@ public class SubClass extends SuperClass {
     }
 
     // перегружаем метод, добавили параметр String s
-    public void invokeInstanceMethod(String s ) {
-        System.out.println("SubClass: overloaded invokeInstanceMethod(String)");
+    public void invokeInstanceMethod(String s) {
+        System.out.println(String.format("SubClass: overloaded invokeInstanceMethod(%s)", s));
+    }
+    public void invokeInstanceMethod(double s) {
+        System.out.println(String.format("SubClass: overloaded invokeInstanceMethod(%.2f)", s));
+    }
+    public void invokeInstanceMethod(int s) {
+        System.out.println(String.format("SubClass: overloaded invokeInstanceMethod(%d)", s));
     }
 }
